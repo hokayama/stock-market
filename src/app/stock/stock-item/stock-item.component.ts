3639d2dbfe25d9bae23f31a746de693c9279a86c
hokayama <hokayama@uol.com.br>
2022-06-27 22:49:44 -0300

    Corrigido 26/06/2022

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,6 +9,7 @@ import { Stock } from 'src/app/model/stock';
 })
 export class StockItemComponent implements OnInit {
 
+/*
   public stocks!: Array<Stock>;
 
   constructor() { }
@@ -25,5 +26,26 @@ export class StockItemComponent implements OnInit {
     console.log('We are toggling the favorite state for this stock.', index, event)
     this.stocks[index].favorite = !this.stocks[index].favorite;
   }
+*/
 
+public stocks!: Array<Stock>;
+
+constructor() { }
+
+ngOnInit() {
+  this.stocks = [
+    new Stock('Test Stock Company', 'TSC', 85, 80),
+    new Stock('Second Stock Company', 'SSC', 10, 20),
+    new Stock('Last Stock Company', 'LSC', 876, 765)
+  ];
+}
+
+toggleFavorite(event, index) {
+  console.log('We are toggling the favorite state for stock', index, event);
+  this.stocks[index].favorite = !this.stocks[index].favorite;
+}
+
+trackStockByCode(index, stock) {
+  return stock.code;
+}
 }
